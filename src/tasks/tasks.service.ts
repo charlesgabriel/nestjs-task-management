24e109dd2312c53cf979b314c89d86565f3f7d8d
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-06 14:16:04 -0300

    refatorado para service/serviço de task/tarefa

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,66 +1,64 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
 
-    getAllTasks(): Task[] {
-        return this.tasks;
-    }
+    // getAllTasks(): Task[] {
+    //     return this.tasks;
+    // }
 
-    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-        const { status, search } = filterDto;
-        // define a temporary array to hold the result
-        let tasks = this.getAllTasks();
-        // do something with status
-        if(status) {
-            tasks = tasks.filter((task) => task.status === status);
-        }
-        // do something with search
-        if(search) {
-            tasks = tasks.filter((task) => {
-                if(task.title.includes(search) || task.description.includes(search)) {
-                    return true;
-                }
-                return false;
-            });
-        }
-        // return final result
-        return tasks;
-    }
+    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    //     const { status, search } = filterDto;
+    //     // define a temporary array to hold the result
+    //     let tasks = this.getAllTasks();
+    //     // do something with status
+    //     if(status) {
+    //         tasks = tasks.filter((task) => task.status === status);
+    //     }
+    //     // do something with search
+    //     if(search) {
+    //         tasks = tasks.filter((task) => {
+    //             if(task.title.includes(search) || task.description.includes(search)) {
+    //                 return true;
+    //             }
+    //             return false;
+    //         });
+    //     }
+    //     // return final result
+    //     return tasks;
+    // }
 
-    getTaskById(id: string): Task {
-        const found = this.tasks.find((task) => task.id === id);
-        if(!found) {
-            throw new NotFoundException(`Task with ID "${id}" not found`);
-        }
-        return found;
-    }
+    // getTaskById(id: string): Task {
+    //     const found = this.tasks.find((task) => task.id === id);
+    //     if(!found) {
+    //         throw new NotFoundException(`Task with ID "${id}" not found`);
+    //     }
+    //     return found;
+    // }
 
-    createTask(createTaskDto: CreateTaskDto): Task {
-        const { title, description } = createTaskDto;
-        const task: Task = {
-            id: uuid(),
-            title,
-            description,
-            status: TaskStatus.OPEN,
-        };
-        this.tasks.push(task);
-        return task;
-    }
+    // createTask(createTaskDto: CreateTaskDto): Task {
+    //     const { title, description } = createTaskDto;
+    //     const task: Task = {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN,
+    //     };
+    //     this.tasks.push(task);
+    //     return task;
+    // }
 
-    deteleTask(id: string): void {
-        const found = this.getTaskById(id);
-        this.tasks = this.tasks.filter((task) => task.id !== found.id);
-    }
+    // deteleTask(id: string): void {
+    //     const found = this.getTaskById(id);
+    //     this.tasks = this.tasks.filter((task) => task.id !== found.id);
+    // }
 
-    updateTaskStatus(id: string, status: TaskStatus): Task {
-        const task: Task = this.getTaskById(id);
-        task.status = status;
-        return task;
-    }
+    // updateTaskStatus(id: string, status: TaskStatus): Task {
+    //     const task: Task = this.getTaskById(id);
+    //     task.status = status;
+    //     return task;
+    // }
 }
