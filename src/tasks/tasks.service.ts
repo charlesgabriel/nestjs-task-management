3430272a6d06bcbd05c7f474171b824364268831
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-07 08:59:07 -0300

    refatorado persistencia criar uma tarefa/persistence creating a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TasksRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { create } from 'domain';
 
 @Injectable()
 export class TasksService {
@@ -46,17 +47,9 @@ export class TasksService {
         return found;
     }
 
-    // createTask(createTaskDto: CreateTaskDto): Task {
-    //     const { title, description } = createTaskDto;
-    //     const task: Task = {
-    //         id: uuid(),
-    //         title,
-    //         description,
-    //         status: TaskStatus.OPEN,
-    //     };
-    //     this.tasks.push(task);
-    //     return task;
-    // }
+    createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksRepository.createTask(createTaskDto);
+    }
 
     // deteleTask(id: string): void {
     //     const found = this.getTaskById(id);
