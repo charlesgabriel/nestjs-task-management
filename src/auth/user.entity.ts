fab42a533f892c692d010842468b5869f869a104
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-12 09:06:33 -0300

    criado entidade usuário/create user entity

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,11 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column()
+    username: string;
+    @Column()
+    password: string;
 }
