29758cdd766715f8ad8536a6db5ccaa52abe3c66
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-20 08:56:52 -0300

    implementado assinatura token jwt na autenticação do login/implemented signing a jwt token on sign in authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ export class AuthController {
         return this.authService.signUp(authCredentialsDto);
     }
     @Post('/signin')
-    signin(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    signin(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
     }
 }
