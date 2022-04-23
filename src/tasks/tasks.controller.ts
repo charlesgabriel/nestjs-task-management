1cd90a6446cf429553a418a1fa2ba0108bb6b545
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-23 08:48:23 -0300

    implementado alguns logs/implemented some logs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,16 +7,19 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+    private logger = new Logger('TasksController');
     constructor(private tasksService: TasksService) {}
 
     @Get()
     getTasks(@Query() filterDto: GetTasksFilterDto, 
         @GetUser() user: User,
     ): Promise<Task[]> {
+        this.logger.verbose(`User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`);
         return this.tasksService.getTasks(filterDto, user);
     }
 
@@ -29,6 +32,7 @@ export class TasksController {
     createTask(@Body() createTaskDto: CreateTaskDto,
         @GetUser() user: User,
     ): Promise<Task> {
+        this.logger.verbose(`Use "${user.username}" creating a new tasks. Data: ${JSON.stringify(createTaskDto)}`);
         return this.tasksService.createTask(createTaskDto, user);
     }
 
